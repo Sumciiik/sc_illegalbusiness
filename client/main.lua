afd21d0a45bdb1d707bf5b4f01806e0e231c5df9
Sumciiik <jackobpeterka@gmail.com>
2025-07-20 21:51:14 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,45 @@
 RegisterNetEvent('sc_illegalbusiness:busdealermenu')
 AddEventHandler('sc_illegalbusiness:busdealermenu', function ()
-    lib.registerContext({
-        id = 'busdealermenu',
-        title = 'What do you want?',
-        options = {
-            {
-                title = 'YES',
-            }
+    bconfirm = lib.alertDialog({
+        header = 'Hey',
+        content = 'I heard that you wanted to buy some shady business.  \n Is it true?',
+        centered = true,
+        cancel = true,
+        labels = {
+            confirm = 'Yes',
+            cancel = 'No',
         }
     })
-    lib.showContext('busdealermenu')
+    
+    if bconfirm == 'confirm' then
+        lib.registerContext({
+            id = 'busdealermenu',
+            title = 'What do you want?',
+            options = {
+                {
+                    title = 'List of businesses',
+                    event = 'sc_illegalbusiness:listbus'
+                }
+            }
+        })
+        lib.showContext('busdealermenu')
+    else
+        lib.notify({
+            title = 'Shady Guy',
+            description = 'Get out of here then.',
+            type = 'warning'
+        })
+    end
+end)
+
+RegisterNetEvent('sc_illegalbusiness:listbus')
+AddEventHandler('sc_illegalbusiness:listbus',function ()
+    lib.registerContext({
+        id = 'busdealerlist',
+        title = 'Here is what I`ve got',
+        options = {
+            
+        }
+    })
+    lib.showContext('busdealerlist')
 end)
